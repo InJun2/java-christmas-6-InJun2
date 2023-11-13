11bcf870ccdd63d8fea7328ff8e72e470e907e9d
InJun2 <os2864@naver.com>
2023-11-14 02:50:40 +0900

    :sparkles: 주문 메뉴 입력 및 해당 Controller 요청

--- src/main/java/christmas/run/PromotionRun.java
+++ src/main/java/christmas/run/PromotionRun.java
@@ -2,32 +2,46 @@ package christmas.run;
 
 import christmas.controller.PromotionController;
 import christmas.model.event.ReservationDateEvent;
+import christmas.model.event.dto.ReservationDateEventDto;
+import christmas.model.menu.dto.OrderMenusDto;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class PromotionRun {
+    private final PromotionController controller = new PromotionController();
     private final InputView inputView;
     private final OutputView outputView;
-    private final PromotionController controller;
 
     public PromotionRun(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        controller = new PromotionController();
     }
 
     public void run() {
-        generateEventByDate();
+        ReservationDateEventDto dateEventDto = generateEventByDate();
+
+        OrderMenusDto orderMenusDto = receiveOrderMenus();
+
+//        -> Dto 이벤트 비교
     }
 
-    private ReservationDateEvent generateEventByDate() {
+    private ReservationDateEventDto generateEventByDate() {
         String inputDate = inputReservationDate();
         return controller.generateEvent(inputDate);
+    }
+
+    private OrderMenusDto receiveOrderMenus() {
+        String inputMenus = inputOrderMenus();
+        return controller.receiveOrderMenus(inputMenus);
     }
 
     private String inputReservationDate() {
         outputView.displayStartPromotion();
         return inputView.inputDate();
+    }
+
+    private String inputOrderMenus() {
+        return inputView.inputOrderMenus();
     }
 
 }
