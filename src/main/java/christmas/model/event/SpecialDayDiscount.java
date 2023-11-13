9746e2500c4c75e4596fadc71a2a2c4fa6bbe13d
InJun2 <os2864@naver.com>
2023-11-14 02:49:23 +0900

    :art: 직관적인 Enum 명으로 변경

--- src/main/java/christmas/model/event/SpecialDayDiscount.java
+++ src/main/java/christmas/model/event/SpecialDayDiscount.java
@@ -1,19 +1,18 @@
 package christmas.model.event;
 
-public enum SpecialDiscountDate {
+public enum SpecialDayDiscount {
     THIRD_DAY(3, 1000),
-    SEVENTH_DAY(7, 1000),
     TENTH_DAY(10, 1000),
     SEVENTEENTH_DAY(17, 1000),
     TWENTY_FOURTH_DAY(24, 1000),
     TWENTY_FIFTH_DAY(25, 1000),
     THIRTY_FIRST_DAY(31, 1000),
-    OTHER_DAY(-1, 0);
+    OTHER_DAY(0, 0);
 
     private final int day;
     private final int discountPrice;
 
-    SpecialDiscountDate(int day, int discountPrice) {
+    SpecialDayDiscount(int day, int discountPrice) {
         this.day = day;
         this.discountPrice = discountPrice;
     }
