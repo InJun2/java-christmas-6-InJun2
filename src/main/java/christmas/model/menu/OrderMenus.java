e606d3f6fd051266a5321d25e933e2c05215c354
InJun2 <os2864@naver.com>
2023-11-15 01:47:13 +0900

    :sparkles: 유효성 검사 및 getter 구현

--- src/main/java/christmas/model/menu/OrderMenus.java
+++ src/main/java/christmas/model/menu/OrderMenus.java
@@ -1,16 +1,92 @@
 package christmas.model.menu;
 
-import java.util.EnumMap;
-import java.util.Map;
+import christmas.exception.ExceptionMessage;
+import christmas.exception.PromotionException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OrderMenus {
+    private static final int MIN_ORDER_MENU_NUMBER = 1;
+    private static final int MAX_ORDER_MENUS_NUMBER = 20;
+    private static final int MIN_ORDER_TOTAL_AMOUNT = 10000;
+
     private final Map<MenuItem, Integer> orderMenus;
 
     public OrderMenus(Map<String, Integer> orderMenus) {
-        this.orderMenus = new EnumMap<>(MenuItem.class);
+        this.orderMenus = validateOrderMenusByNames(orderMenus);
+        validateOrderMenus();
     }
 
-    public Map<MenuItem, Integer> getOrderMenus() {
-        return new EnumMap<>(orderMenus);
+    private Map<MenuItem, Integer> validateOrderMenusByNames(Map<String, Integer> orderMenus) {
+        return generateOrderMenuEntry(orderMenus)
+                .collect(Collectors.toUnmodifiableMap(
+                        entry -> MenuItem.findMenuItemByName(entry.getKey()),
+                        Map.Entry::getValue
+                ));
+    }
+
+    private void validateOrderMenus() {
+        validateOrderNumbers();
+        validateTotalOrderNumbers();
+        validateOrderAllBeverage();
+        validateTotalOrderAmount();
+    }
+
+    private void validateOrderNumbers() {
+        generateOrderMenuEntry(orderMenus)
+                .filter(menu ->
+                        menu.getValue() < MIN_ORDER_MENU_NUMBER)
+                .findAny()
+                .ifPresent(menuName -> throwInvalidOrderException());
+    }
+
+    private void validateTotalOrderNumbers() {
+        int orderNumber = generateOrderMenuEntry(orderMenus)
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+
+        if (orderNumber > MAX_ORDER_MENUS_NUMBER) {
+            throwInvalidOrderException();
+        }
+    }
+
+    private void validateOrderAllBeverage() {
+        boolean allBeverage = generateOrderMenuEntry(orderMenus)
+                .allMatch(orderMenu ->
+                        orderMenu.getKey().getMenuCategory() == MenuCategory.BEVERAGE);
+
+        if (allBeverage) {
+            throwInvalidOrderException();
+        }
+    }
+
+    private void validateTotalOrderAmount() {
+        int totalPrice = generateOrderMenuEntry(orderMenus)
+                .mapToInt(orderMenu ->
+                        orderMenu.getKey().getItemPrice() * orderMenu.getValue())
+                .sum();
+
+        if (totalPrice < MIN_ORDER_TOTAL_AMOUNT) {
+            throwInvalidOrderException();
+        }
+    }
+
+    private <K, V> Stream<Map.Entry<K, V>> generateOrderMenuEntry(Map<K, V> orderMenus) {
+        return orderMenus.entrySet()
+                .stream();
+    }
+
+    private void throwInvalidOrderException() {
+        throw new PromotionException(ExceptionMessage.INVALID_INPUT_ORDER);
+    }
+
+    public Map<String, Integer> getOrderMenus() {
+        Map<String, Integer> menuNames = new HashMap<>();
+        orderMenus.forEach((menuItem, quantity) ->
+                menuNames.put(menuItem.getItemName(), quantity));
+
+        return Collections.unmodifiableMap(menuNames);
     }
 }
