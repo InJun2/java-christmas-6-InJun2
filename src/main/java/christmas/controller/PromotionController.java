edb23865adb487f46042406f55c93cfedfd4a722
InJun2 <os2864@naver.com>
2023-11-15 01:47:30 +0900

    :sparkles: 이벤트 실행 요청 추가

--- src/main/java/christmas/controller/PromotionController.java
+++ src/main/java/christmas/controller/PromotionController.java
@@ -1,17 +1,22 @@
 package christmas.controller;
 
-import christmas.model.event.dto.ReservationDateEventDto;
-import christmas.model.menu.dto.OrderMenusDto;
+import christmas.model.event.dto.EventResultDTO;
+import christmas.model.event.dto.ReservationDateEventDTO;
+import christmas.model.menu.dto.OrderMenusDTO;
 import christmas.service.PromotionService;
 
 public class PromotionController {
     private final PromotionService service = new PromotionService();
 
-    public ReservationDateEventDto generateEvent(String inputDate) {
+    public ReservationDateEventDTO generateEvent(String inputDate) {
         return service.generateEvent(inputDate);
     }
 
-    public OrderMenusDto receiveOrderMenus(String inputMenus) {
+    public OrderMenusDTO receiveOrderMenus(String inputMenus) {
         return service.receiveOrderMenus(inputMenus);
+    }
+
+    public EventResultDTO applyEvents(ReservationDateEventDTO dateEventDTO, OrderMenusDTO orderMenusDto) {
+        return service.applyEvents(dateEventDTO, orderMenusDto);
     }
 }
