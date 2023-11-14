87c781b833cc1a504172f62010eab0ca6760e54b
InJun2 <os2864@naver.com>
2023-11-15 01:47:44 +0900

    :sparkles: 이벤트 DTO 변경 추가

--- src/main/java/christmas/converter/PromotionConverter.java
+++ src/main/java/christmas/converter/PromotionConverter.java
@@ -1,16 +1,36 @@
 package christmas.converter;
 
+import christmas.model.event.EventResult;
 import christmas.model.event.ReservationDateEvent;
-import christmas.model.event.dto.ReservationDateEventDto;
+import christmas.model.event.dto.EventResultDTO;
+import christmas.model.event.dto.ReservationDateEventDTO;
+import christmas.model.menu.MenuItem;
 import christmas.model.menu.OrderMenus;
-import christmas.model.menu.dto.OrderMenusDto;
+import christmas.model.menu.dto.OrderMenusDTO;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PromotionConverter {
-    public ReservationDateEventDto toDto(ReservationDateEvent event) {
-        return new ReservationDateEventDto(event);
+    public ReservationDateEventDTO toDto(ReservationDateEvent event) {
+        return new ReservationDateEventDTO(event);
     }
 
-    public OrderMenusDto toDto(OrderMenus orderMenus) {
-        return new OrderMenusDto(orderMenus);
+    public OrderMenusDTO toDto(OrderMenus orderMenus) {
+        return new OrderMenusDTO(orderMenus);
+    }
+
+    public EventResultDTO toDto(EventResult eventResult) {
+        return new EventResultDTO(eventResult);
+    }
+
+    public Map<MenuItem, Integer> convertToMenuItem(OrderMenusDTO orderMenusDTO) {
+        return orderMenusDTO.getOrderMenus()
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        entry -> MenuItem.findMenuItemByName(entry.getKey()),
+                        Map.Entry::getValue
+                ));
     }
 }
