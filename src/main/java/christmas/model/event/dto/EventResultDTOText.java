059380b496cc28f51c34d8deec1b69883f30fe12
InJun2 <os2864@naver.com>
2023-11-15 01:53:51 +0900

    :sparkles: DTO 출력 문자열 조작을 위한 Enum 구현

---
+++ src/main/java/christmas/model/event/dto/EventResultDTOText.java
@@ -0,0 +1,25 @@
+package christmas.model.event.dto;
+
+enum EventResultDTOText {
+    EMPTY_TEXT(""),
+    MENU_NUMBER("개"),
+    MENU_PRICE_UNIT ("원"),
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인"),
+    SPECIAL_DISCOUNT("특별 할인"),
+    GIFT_EVENT ("증정 이벤트"),
+    NONE_BENEFIT ("없음"),
+    DISCOUNT_PRICE ("-"),
+    BENEFIT_SEPARATOR(": "),
+    NEW_LINE("\n"),
+    ;
+
+    private final String text;
+
+    EventResultDTOText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
