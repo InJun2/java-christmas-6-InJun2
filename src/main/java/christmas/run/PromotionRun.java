0579320d8b9dc34e8b4fe4f0c6ca64f27f17098d
InJun2 <os2864@naver.com>
2023-11-15 13:41:04 +0900

    :sparkles: 예외 발생시 출력 및 메소드 재실행 로직 추가

--- src/main/java/christmas/run/PromotionRun.java
+++ src/main/java/christmas/run/PromotionRun.java
@@ -1,11 +1,14 @@
 package christmas.run;
 
 import christmas.controller.PromotionController;
+import christmas.exception.PromotionException;
 import christmas.model.event.dto.EventResultDTO;
 import christmas.model.event.dto.ReservationDateEventDTO;
 import christmas.model.menu.dto.OrderMenusDTO;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+
+import java.util.function.Supplier;
 
 public class PromotionRun {
     private final PromotionController controller = new PromotionController();
@@ -17,28 +20,39 @@ public class PromotionRun {
         this.outputView = outputView;
     }
 
-    public void run() {
-        ReservationDateEventDTO dateEventDto = generateEventByDate();
-        OrderMenusDTO orderMenusDto = receiveOrderMenus();
+    public void runPromotion() {
+        ReservationDateEventDTO dateEventDto = inputCorrectReservationDate();
+        OrderMenusDTO orderMenusDto = inputCorrectOrderMenus();
+        displayEventPreview(dateEventDto);
 
         EventResultDTO responseDto = applyEventsAndGenerateResult(dateEventDto, orderMenusDto);
-        System.out.println(responseDto.getBenefitHistory());
-        System.out.println(responseDto.getOrderMenus());
-        System.out.println(responseDto.getGiftMenu());
-        System.out.println(responseDto.getTotalBenefit());
-        System.out.println(responseDto.getTotalPriceBeforeDiscount());
-        System.out.println(responseDto.getTotalPriceAfterDiscount());
-        System.out.println(responseDto.getRewardBadge());
+        outputEventResult(responseDto);
     }
 
-    private ReservationDateEventDTO generateEventByDate() {
-        String inputDate = inputReservationDate();
+    private ReservationDateEventDTO inputCorrectReservationDate() {
+        outputView.displayStartPromotion();
+
+        return getCorrectResult(this::generateDateEvent);
+    }
+
+    private OrderMenusDTO inputCorrectOrderMenus() {
+        return getCorrectResult(this::receiveMenus);
+    }
+
+    private ReservationDateEventDTO generateDateEvent() {
+        String inputDate = inputView.inputDate();
+
         return controller.generateEvent(inputDate);
     }
 
-    private OrderMenusDTO receiveOrderMenus() {
-        String inputMenus = inputOrderMenus();
+    private OrderMenusDTO receiveMenus() {
+        String inputMenus = inputView.inputOrderMenus();
+
         return controller.receiveOrderMenus(inputMenus);
+    }
+
+    private void displayEventPreview(ReservationDateEventDTO dateEventDto) {
+        outputView.displayPreviewEvent(dateEventDto.getDay());
     }
 
     private EventResultDTO applyEventsAndGenerateResult(ReservationDateEventDTO dateEventDTO,
@@ -46,12 +60,17 @@ public class PromotionRun {
         return controller.applyEvents(dateEventDTO, orderMenusDto);
     }
 
-    private String inputReservationDate() {
-        outputView.displayStartPromotion();
-        return inputView.inputDate();
+    private void outputEventResult(EventResultDTO responseDto) {
+        outputView.outputEventResult(responseDto);
     }
 
-    private String inputOrderMenus() {
-        return inputView.inputOrderMenus();
+    private <T> T getCorrectResult(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (PromotionException e) {
+                outputView.displayException(e);
+            }
+        }
     }
 }
